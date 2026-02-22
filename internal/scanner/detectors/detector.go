b5b77accc11b7e1c0d8e2d805abae2eb6cd51ef2
Melon <80179069+OfficiallyMelon@users.noreply.github.com>
2026-02-22 14:09:18 +1000

    repeating chars

--- internal/scanner/detectors/detector.go
+++ internal/scanner/detectors/detector.go
@@ -6,11 +6,31 @@ type DetectorFunc func(src string) (string, bool, string)
 
 var AllDetectors []DetectorFunc
 
+// kill me plz uwu
+func hasRepeatingChars(s string, threshold int) bool {
+	if len(s) == 0 {
+		return false
+	}
+	count := 1
+	for i := 1; i < len(s); i++ {
+
+		if s[i] == s[i-1] {
+
+			count++
+
+			if count >= threshold {
+				return true
+			}
+
+		} else {
+			count = 1
+		}
+	}
+	return false
+}
+
 func EnsureKeyIsntSpam(key string) bool {
 	lower := strings.ToLower(key)
-	if strings.Count(lower, "x") >= 6 {
-		return false // spam
-	}
 	if strings.Contains(lower, "your_api_key") || strings.Contains(lower, "placeholder") {
 		return false // spam
 	}
@@ -32,7 +52,7 @@ func EnsureKeyIsntSpam(key string) bool {
 	if strings.Contains(lower, "token") {
 		return false
 	}
-	if strings.Contains(lower, "000000") {
+	if hasRepeatingChars(lower, 6) {
 		return false
 	}
 
